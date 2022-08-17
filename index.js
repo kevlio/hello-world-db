dc6f02f0e48f278815e66b5c13bb48234d40661b
Lundberg <kevin.lundberg@sweco.se>
2022-08-17 11:56:12 +0200

    db migration

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const app = require("express")();
 
 const config = require("./knexfile.js");
-const knex = require("knex")(/* Knexfile-config */)
+const knex = require("knex")(config[process.env.NODE_ENV]);
 
 app.get("/", (req, res) => {
   res.send(process.env.GREETING);
-})
+});
 
 app.get("/add/:name", (req, res) => {
   // Ersätt person med den skapade personen från databasen
   const person = {
     id: 0,
-    name: "Ryan"
-  }; 
+    name: "Ryan",
+  };
 
-  res.send(person)
-})
+  res.send(person);
+});
 
 app.get("/list", (req, res) => {
   // Ersätt people med alla personer från databasen
-  const people = [{
-    id: 0,
-    name: "Ryan"
-  }];
+  const people = [
+    {
+      id: 0,
+      name: "Ryan",
+    },
+  ];
 
   res.send(people);
-})
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`)
-})
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
--- knexfile.js
+++ knexfile.js
@@ -4,44 +4,34 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: "./dev.sqlite3",
+    },
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
