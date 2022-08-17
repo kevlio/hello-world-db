bd75b7fcd2fbd929b99dcd79be9f29c0c70e2264
Lundberg <kevin.lundberg@sweco.se>
2022-08-17 13:17:53 +0200

    added migration

--- migrations/20220817095424_first_migration.js
+++ migrations/20220817095424_first_migration.js
@@ -2,14 +2,25 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("tableName", (table) => {
+    // "På tabellen" lägger vi till "kolumntypen" med namnet "name"
+    table.columntype("name");
+    // ^ Endast exempel på ordningen
+
+    // På tabellen, lägger vi till en kolumn av knex-typen "increments"
+    // med namnet id. Som ger oss en kolumn med ett id som ökar för varje rad
+    // vi lägger till i tabellen.
+    table.increments("id");
+    // Och en kolumn för namn, av typen string
+    table.string("name");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("tableName");
 };
